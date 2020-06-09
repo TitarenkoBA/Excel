fd952f5a6701957a4d101de10dbaa60c928560fa
Boris Titarenko <zik2009@yandex.ru>
2020-06-09 17:19:49 +0400

    add DomListeners, dom, utils

---
+++ src/core/DomListener.js
@@ -0,0 +1,30 @@
+import { capitalize } from '@core/utils'
+export class DomListener {
+  constructor($root, listeners = []) {
+    if (!$root) {
+      throw new Error('No $root provided for DomListener!')
+    }
+
+    this.$root = $root
+    this.listeners = listeners
+  }
+
+  initDomListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      if (!this[method]) {
+        throw new Error(`Method ${method} 
+          is not implemented in ${this.name || ''} Component`)
+      }
+      this.$root.on(listener, this[method].bind(this))
+    })
+  }
+
+  removeDomListeners() {
+
+  }
+}
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName)
+}
